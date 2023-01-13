643338c3645cc7513936bd74bc06c254127120ab
acoolguy12 <122555202+acoolguy12@users.noreply.github.com>
2023-01-13 12:19:04 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Skull)
-    music.playMelody("B G D F - E F C ", 120)
+    music.playMelody("C G D F - E F A ", 200)
 })
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Ghost)
+    for (let index = 0; index < 50; index++) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1960, 5000, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
 })
+music.playMelody("E D G C E F G C ", 120)
 basic.showLeds(`
     # . # . #
     . # # # .
